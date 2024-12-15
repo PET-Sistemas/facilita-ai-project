c8326e9385b598b06f38b55c65f3bcd1477d2f22
Gabriel-Sena-1 <gabrielmirandasena11@gmail.com>
2024-12-15 14:34:51 -0400

    fix: change enpoint to api

--- front-facilita_ai/src/services/api.js
+++ front-facilita_ai/src/services/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080'; // Substitua pelo URL do seu back-end
+const API_URL = 'http://localhost:8080/api'; // Substitua pelo URL do seu back-end
 
 export const login = async (email, password) => {
     try {
