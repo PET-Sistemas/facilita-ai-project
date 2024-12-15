fa4808453fe8f45d8ab51b35857ea8caedc6eb4e
Gabriel-Sena-1 <gabrielmirandasena11@gmail.com>
2024-12-14 20:54:56 -0400

    fix: change allowed origin

--- getpet/src/main/config/WebConfig.java
+++ getpet/src/main/config/WebConfig.java
@@ -8,7 +8,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Aplica CORS a todos os endpoints
-                .allowedOrigins("http://localhost:3000") // Origem permitida (frontend)
+                .allowedOrigins("http://151.106.108.50:3001") // Origem permitida (frontend)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
                 .allowedHeaders("*") // Permitir qualquer cabeçalho
                 .allowCredentials(true); // Permitir envio de cookies/autenticação
