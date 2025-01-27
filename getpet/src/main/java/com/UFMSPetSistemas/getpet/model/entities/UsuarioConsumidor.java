a8e980fbcaa411f4148ab55ea71d30f053e4130b
CaueMoraes1 <cauemor@gmail.com>
2025-01-27 18:03:37 -0400

    fix: apagando antiga logica do back-end

--- getpet/src/main/java/com/UFMSPetSistemas/getpet/model/entities/UsuarioConsumidor.java
+++
@@ -1,9 +0,0 @@
-package com.UFMSPetSistemas.getpet.model.entities;
-
-import com.UFMSPetSistemas.getpet.model.entities.PrestacaoServico;
-
-public class UsuarioConsumidor extends Usuario {
-
-	private PrestacaoServico prestacaoServico;
-
-}
--- getpet/src/main/java/com/UFMSPetSistemas/getpet/model/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.UFMSPetSistemas.getpet.model.repository;
-
-import com.UFMSPetSistemas.getpet.model.entities.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-    //public Usuario findByGerenteId(Integer idGerente);
-}
